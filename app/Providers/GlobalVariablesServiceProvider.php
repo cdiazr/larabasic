82f0bbe3d462d1bf4717724d554071b169958982
Carlos Diaz <cdiazr@ymail.com>
2023-05-04 09:59:35 +0200

    Fix issue
    Check if "settings" table exists.

--- app/Providers/GlobalVariablesServiceProvider.php
+++ app/Providers/GlobalVariablesServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -25,16 +26,20 @@ class GlobalVariablesServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $settings = $this->buildArraySettings();
-        View::composer('*', function ($view) use($settings) {
-            $view->with('setting_maintenance', $settings['maintenance']);
-            $view->with('setting_master', $settings['master']);
-            $view->with('setting_account_target', $settings['target']);
-            $view->with('setting_languages', $settings['languages']);
-            $view->with('setting_footer', $settings['footer_fix']);
-            $view->with('setting_navbar_color', $settings['navbar_color']);
-            $view->with('setting_sidebar_color', $settings['sidebar_color']);
-        });
+        $flag = Schema::hasTable('settings');
+
+        if($flag) {
+            $settings = $this->buildArraySettings();
+            View::composer('*', function ($view) use($settings) {
+                $view->with('setting_maintenance', $settings['maintenance']);
+                $view->with('setting_master', $settings['master']);
+                $view->with('setting_account_target', $settings['target']);
+                $view->with('setting_languages', $settings['languages']);
+                $view->with('setting_footer', $settings['footer_fix']);
+                $view->with('setting_navbar_color', $settings['navbar_color']);
+                $view->with('setting_sidebar_color', $settings['sidebar_color']);
+            });
+        }
     }
 
     private function buildArraySettings()
